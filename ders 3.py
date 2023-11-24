2b53278f8016395f643f15aa5b3f710e909ce40f
Esrakblt <151937076+Esrakblt@users.noreply.github.com>
2023-11-24 16:52:50 +0300

    ortalama ders eklendi

---
+++ ders 3.py
@@ -0,0 +1,73 @@
+# Alıştırma : dikdörtgen çevresi ve alani hesabı
+# kallanıcıdan uzun ve kısa kenar girdisi alalım
+# kullanıcıya bu dikdörtgen alanı ve çevresini göstereceksiniz.
+
+uzunKenar= float(input("dikdörtgenin uzun Kenari :"))
+kisaKenar= float(input("Dikdörtgenin Kisa Kenari :"))
+
+# Matematiksel formüller
+# alan hesabi uzunKenar * kısaKenar
+# cevre hesabi 2 * (uzunKenar + kisaKenar)
+
+print("---Dikdortgen Alani ve Cevresi---")
+print
+
+baslangic=int(input("baslangic degerini giriniz:"))
+bitis=int(input("bitis degerini giriniz:"))
+print("Sansli Sayi Oyununa Hosgeldiniz!")
+print("2 ile 70 arasında bir sayi seçildi !")
+tahmin=int(input("Sayiyi Tahmin Ediniz:")
+if tahmin == sansli_sayi:
+    print("Tebrikler  ! Oyunu ")
+else:
+    print(f
+
+
+
+
+yas=input("yasinizi giriniz:")
+ad=input("adinizi giriniz:")
+sonuc= yas +ad
+print(f"toplam{sonuc}")
+
+
+
+sehir=input("Bulundugunuz Sehrin Adini giriniz:")
+print("Sehir:",sehir)
+
+en_yuksek_sicaklik=float(input("Bugun Gozlemlenen En Yuksek Sicaklik :"))
+en_dusuk_sicaklik=float(input("Bugun Gozlemlenen
+ortalama_sicaklik=(en_yuksek_sicaklik+en_dusuk_sicaklik)/2
+print(f"(sehir) şehrindeki günlük sicaklik:{ortalama_sicaklik:.2f})
+
+
+
+
+
+
+
+
+#Girilen Sayinin n^2 n^3 n^4 degerlerinin hesaplayan uygulama
+
+sayi=int(input("Bir Sayi giriniz:"))
+kare= sayi ** 2
+kup= sayi ** 3
+n4= sayi ** 4
+
+print(f"Girdiginiz Sayinin Karesi: {kare}")
+print(f"Girdiginiz Sayinin Küpü: {kup}")
+print(f"Girdiginiz Sayinin n^4 ü: {n4}")
+
+
+
+vize1=float(input("vize1 Notunuzu giriniz:"))
+vize2=float(input("vize2 de""u giriniz:"))
+vize3=float(input(vize3 Notunuzu giriniz:"))
+final=float(input(
+not_ortalamasi=(vize1*20+ vize2*20+vize3* 20+final *40)/100
+
+
+if not_ortalamasi>=60 and not_ortalamasi<=100
+
+
+
